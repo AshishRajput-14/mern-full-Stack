6379091a42210c9f742ed6f0a52d3e19fd74dbf5
AshishRajput-14 <ar8386829@gmail.com>
2025-11-19 18:15:32 +0530

    complete

---
+++ javaScript/index.js
@@ -0,0 +1,18 @@
+var a =10;
+
+
+var a=20;
+
+
+let b=10;
+
+
+console.log(a,b);
+
+let ar = ["abc",1235,25,22,30,'a', true]
+console.log(ar)
+
+console.warn("this is warinng")
+console.error("ye error hai =")
+
+
---
+++ loginbootstrap/script.js
@@ -0,0 +1,21 @@
+function login(){
+    console.log("LOgin button clicked");
+
+   const em = document.getElementById("LoginEmail")
+   const ps = document.getElementById("LoginPassword")
+   console.log("Email:"+em)
+console.log("Email:"+ps) 
+
+alert("Login Done");
+    document.getElementById("LoginEmail").value = "";
+
+    document.getElementById("LoginPassword").value = "";
+
+}
+
+
+
+function registration(){
+    console.log("Registration button clicked")
+    
+}
