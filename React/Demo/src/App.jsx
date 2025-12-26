539cbe31b4ae3e06c9299c0f46b9ee7710ca70f8
AshishRajput-14 <ar8386829@gmail.com>
2025-12-26 12:00:03 +0530

    done

---
+++ React/Demo/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function App(){
+  let a = 5;
+  let b = 10;
+
+  console.log(a+b);
+
+  return (
+    <>
+<h1 id="head">Hello Word</h1>
+<div>
+  this is my frist React App
+</div>
+    </>
+  );
+
+}
+
+export default App;
