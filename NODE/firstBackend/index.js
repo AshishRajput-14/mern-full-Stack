f70d17d37fa67c4f8738d900b856775494f005f7
AshishRajput-14 <ar8386829@gmail.com>
2026-01-10 17:53:19 +0530

    commit

--- NODE/firstBackend/index.js
+++ NODE/firstBackend/index.js
@@ -2,18 +2,28 @@ import dotenv from'dotenv'
 dotenv.config();
 
 import express from 'express'
+import connectDB from './src/config/db.js';
+import AuthRouter from "./src/routers/myRouter.js"
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/auth",AuthRouter);
+
+
+
+
 
 app.get("/",(req,res)=>{
 
     console.log("server is Running");
     res.json({message:"sever is running sucesfully"});
-})
+});
 
 const port = process.env.port || 5000;
 app.listen(port,()=>{
-    console.log("server Started at pott",port);
+    console.log("server Started at pott",port); 
 
-})
+});
 
---
+++ NODE/firstBackend/src/models/userModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    fullName:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type: String,
+        required:true,
+       // unique:true,
+    },
+    phone:{
+        type: String,
+        required:true,
+        //unique:true,
+    
+    },
+    password:{
+        type: String,
+        required:true,
+        
+    
+        
+    },
+    
+},
+{timestampes: true}
+);
+
+const User = mongoose.model("User",userSchema);
+
+export default User;
